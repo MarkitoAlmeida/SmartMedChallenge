84ebf65fc3f5ddca4a0f8cf4229414fe7a8cac5a
Marcos Almeida <mpalmeida88@gmail.com>
2022-06-22 17:29:15 +0100

    Application context creation

---
+++ SmartMedChallenge.Data/Context/SmartMedChallengeContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SmartMedChallenge.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMedChallenge.Data.Context
+{
+    public class SmartMedChallengeContext : DbContext
+    {
+        public SmartMedChallengeContext(DbContextOptions<SmartMedChallengeContext> options) : base(options) { }
+        public SmartMedChallengeContext() { }
+
+        public DbSet<Medication> Medications { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            // Add mapping
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
