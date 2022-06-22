de794c1784e441be3797368d2c6132b3f33fca49
Marcos Almeida <mpalmeida88@gmail.com>
2022-06-22 17:26:05 +0100

    Model/Core design.

---
+++ SmartMedChallenge.Domain/Models/Medication.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartMedChallenge.Domain.Models
+{
+    public class Medication : Entity
+    {
+        #region Constructor
+
+
+
+        #endregion
+
+        #region Properties
+
+        public string Name { get; private set; }
+        public int Quantity { get; private set; }
+
+        #endregion
+
+        #region Methods
+
+
+
+        #endregion
+    }
+}
