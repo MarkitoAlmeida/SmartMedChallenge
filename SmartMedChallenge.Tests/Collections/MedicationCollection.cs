a86e6363e01ba61e8ba81c315aa8b4ec54d0261e
Marcos Almeida <mpalmeida88@gmail.com>
2022-06-24 15:35:05 +0100

    Configuração e criação de Teste Unitário para criação de novo medicamento.

---
+++ SmartMedChallenge.Tests/Collections/MedicationCollection.cs
@@ -0,0 +1,10 @@
+﻿using SmartMedChallenge.Tests.Fixtures;
+using Xunit;
+
+namespace SmartMedChallenge.Tests.Collections
+{
+    [CollectionDefinition(nameof(MedicationCollection))]
+    public class MedicationCollection : ICollectionFixture<MedicationTestsFixture>
+    {
+    }
+}
